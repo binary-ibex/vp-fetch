3174e15cfab2783b2281ad1024977d0c340ea496
binary-ibex <70746835+binary-ibex@users.noreply.github.com>
2020-11-04 19:06:02 +0530

    add main methods

--- vp-fetch.py
+++ vp-fetch.py
@@ -2,11 +2,28 @@
 import bullet
 import pandas as pd
 import requests
-
+import io
+import base64
 # request header
 headers = {
     "User-Agent": "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html"
 }
+
+def decode_data(coded_string):
+    """return the base64 decoded string"""
+    return base64.b64decode(coded_string)
+
+
+def write_to_file(name, coded_string):
+    """write the data to the .ovpn file"""
+    name = f"{name}.ovpn"
+
+    with open(name, "wb") as fd:
+        fd.write(coded_string)
+
+    print(f"\nThe file is saved with the name => {name}")
+
+
 
 if __name__ == "__main__":
 
@@ -19,5 +36,31 @@ if __name__ == "__main__":
     # reading the csv data
     data = pd.read_csv(io.BytesIO(csv_content), skiprows=1)
 
+    #remove the nan
+    data = data.loc[data['CountryLong'].notna()]
+
+
     # Available coutries
-    print(list(data["CountryShort"]))
+    print(set(data["CountryLong"]))
+
+    # select the name of counrty
+    select = input("\nEnter the desired country name from list above :")
+
+    selected = data[data["CountryLong"] == select]
+
+    if not selected.empty:
+        # sort according to the Score
+        selected = selected.sort_values("Score")
+
+
+        # select the first entry with the best score
+        base_code = selected.iloc[0, -1]
+
+
+        # decode base code
+        base_code = decode_data(base_code)
+
+        # write the *.ovpn file in current dir
+        write_to_file(select, base_code)
+    else:
+        print("Incorrect country name !!")
